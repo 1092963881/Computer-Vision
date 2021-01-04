00bf39901fb7a9d21a4203b4fe3c3ff6408ce0d7
cometjwh <58330448+1092963881@users.noreply.github.com>
2021-01-04 22:50:57 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,13 @@
+// 这是一个调试测试文件，您可以直接运行检测配置是否生�?
+// 若配置任然无法调试，请查阅README_inProject.md
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+    
+    {
+    printf("Congratulasions! It works!\n");
+    system("pause");
+    return 0;
+}
